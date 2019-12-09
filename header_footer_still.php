725eca008182f2ce643113fe8f947408f48765ff
Luciano dii Souza <lucianodiisouza@hotmail.com>
2019-12-09 01:18:51 -0300

    Update and rename header_footer_still.html to header_footer_still.php
    ao adicionar esse hook e essa function ao functions.php eu consigo apenas colocar uma variavel login com uma querystring failed via GET, e com base nisso, fizemos as tratativas no Blog.

--- header_footer_still.php
+++ header_footer_still.php
@@ -1,3 +1,16 @@
+<?php 
+// adicionar ao functions.php 
+add_action( 'wp_login_failed', 'my_front_end_login_fail' );  // hook failed login
+    
+    function my_front_end_login_fail( $username ) {
+       $referrer = $_SERVER['HTTP_REFERER'];  // where did the post submission come from?
+       // if there's a valid referrer, and it's not the default log-in screen
+       if ( !empty($referrer) && !strstr($referrer,'wp-login') && !strstr($referrer,'wp-admin') ) {
+          wp_redirect( $referrer . '?login=failed' );  // let's append some information (login=failed) to the URL for the theme to use
+          exit;
+       }
+    }
+?>
 <style>
     .uf {
         height: 400px !important;
