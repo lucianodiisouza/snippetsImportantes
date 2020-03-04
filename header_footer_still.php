4fb02ca96f8aed1ea83744d021777021970b79ce
Luciano dii Souza <lucianodiisouza@hotmail.com>
2020-03-04 01:09:50 -0300

    Update header_footer_still.php
    add this part on epico theme index.php file to route the requisition at addreess bar to correct place and to display modal correctly when user clicks at protected link

--- header_footer_still.php
+++ header_footer_still.php
@@ -1,4 +1,86 @@
+
+<!-- add this code at index.php on epico folders theme -->
 <?php 
+        
+        if(isset($_GET['login'])){
+            $redirect = $_GET['redirect_to'];
+            
+            echo "
+                <style>
+                    #sombra{
+                        display: flex;
+                    }
+                </style>
+            ";
+        }else{
+            echo "
+                <style>
+                    #sombra{
+                        display: none;
+                    }
+                </style>
+            ";
+        }
+        if(isset($_GET['validate'])){
+            $redirect = $_GET['redirect_to'];
+            
+            echo "
+                <style>
+                    #sombraValidate{
+                        display: block;
+                    }
+                </style>
+            ";
+        }else{
+            echo "
+                <style>
+                    #sombraValidate{
+                        display: none;
+                    }
+                </style>
+            ";
+        }
+         if(isset($_GET['passwordRecovery'])){
+            $redirect = $_GET['redirect_to'];
+            
+            echo "
+                <style>
+                    #sombraRecover{
+                        display: block;
+                    }
+                </style>
+            ";
+        }else{
+            echo "
+                <style>
+                    #sombraRecover{
+                        display: none;
+                    }
+                </style>
+            ";
+        }
+        if(isset($_GET['login'])){
+            echo "
+                <style>
+                    #wrongCredentials{
+                        display: block;
+                    }
+                </style>
+            ";
+        }else{
+            echo "
+                <style>
+                    #wrongCredentials{
+                        display: none;
+                    }
+                </style>
+            ";
+        }
+    ?>
+
+//
+<?php 
+
 // adicionar ao functions.php 
 add_action( 'wp_login_failed', 'my_front_end_login_fail' );  // hook failed login
     
